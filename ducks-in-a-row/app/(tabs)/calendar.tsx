80baba3ff3c87a2c3434f439e35bfa217744755a
Sofia Lynch <113479969+SofiaBLynch@users.noreply.github.com>
2026-02-28 10:22:15 -0500

    Calendar UI WIP, Added Modal Form skeleton

--- ducks-in-a-row/app/(tabs)/calendar.tsx
+++ ducks-in-a-row/app/(tabs)/calendar.tsx
@@ -1,42 +1,61 @@
-import { Calendar, Mode } from 'react-native-big-calendar'
-import { StyleSheet, Dimensions, TouchableOpacity, Modal} from 'react-native';
+import { Calendar, CalendarEvent, ICalendarEventBase, Mode } from 'react-native-big-calendar'
+import { StyleSheet, TouchableOpacity, ScrollView, LayoutChangeEvent, Button, Platform} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useState } from 'react';
-// import { View } from 'react-native-reanimated/lib/typescript/Animated';
 import { View, Text } from 'react-native';
-import { Button, Header } from '@react-navigation/elements';
-import Octicons from "@expo/vector-icons/Octicons";
-import ModalForm from '@/components/modal-form';
 import { ThemedText } from '@/components/themed-text';
-const {height: SCREEN_HEIGHT} = Dimensions.get('window');
+import { EventTile } from '@/components/event-tile';
+import ModalCalendarForm from '@/components/modal-calendar-form';
+
 const events = [
   {
     title: 'Meeting',
     start: new Date(2026, 1, 18, 10, 0),
     end: new Date(2026, 1, 18, 12, 30),
+    description: "a",
+    needsApproval: ['dadf'],
   },
   {
     title: 'Twerk',
     start: new Date(2026, 1, 19, 8, 0),
-    end: new Date(2026, 1, 19, 12, 30),
+    end: new Date(2026, 1, 19, 14, 30),
+    description: "b",
+    needsApproval: ['me'],
   },
   {
     title: 'lil break',
     start: new Date(2026, 1, 19, 12, 35),
     end: new Date(2026, 1, 19, 13, 30),
+    description: "c",
+    needsApproval: ['sd'],
   },
   {
     title: 'Twerk',
     start: new Date(2026, 1, 19, 13, 35),
     end: new Date(2026, 1, 19, 20, 30),
+    description: "d",
+    needsApproval: [],
   },
   {
     title: 'Coffee break',
-    start: new Date(2020, 1, 11, 15, 45),
-    end: new Date(2020, 1, 11, 16, 30),
+    start: new Date(2026, 2, 31, 15, 45),
+    end: new Date(2026, 2, 31, 16, 30),
+    description: 'what is up', 
+    needsApproval: ['me'],
   },
-  
+  {
+    title: 'Multi DAY',
+    start: new Date(2026, 2, 2, 15, 45),
+    end: new Date(2026, 2, 31, 16, 30),
+    description: 'what is up', 
+    needsApproval: ['me', 'you'],
+  },
 ]
+export interface CalendarEvent extends ICalendarEventBase {
+  description: string;
+  needsApproval:any;
+
+}
 
 export default function CalendarPage() {
     const abbrMonth = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
@@ -44,7 +63,15 @@ export default function CalendarPage() {
     const[currentDate, setCurrentDate] = useState(new Date());
     const[currentMonth, setCurrentMonth] = useState(abbrMonth[currentDate.getMonth()]);
     const[currentMode, setCurrentMode] = useState<Mode>('week');
-    const[addVisible, setAddVisible] = useState(false);
+    const[calendarHeight, setCalendarHeight] = useState(0);
+    const [showCalendar, setShowCalendar] = useState(true);
+    const [showEvents, setShowEvents] = useState(false);
+    const [editModal, setEditModal] = useState(false);
+    const [event, setEvent] = useState<CalendarEvent|null>(null);
+    const calendarLayout = (e:LayoutChangeEvent) => {
+      const{height} = e.nativeEvent.layout;
+      setCalendarHeight(height);
+    }
     function changeView(date: Date, switchView: Boolean) {
         if(switchView === true) {
             setCurrentMode(currentMode==='week'? 'month' : 'week' );
@@ -52,10 +79,24 @@ export default function CalendarPage() {
         setCurrentDate(date);
         setCurrentMonth(abbrMonth[date.getMonth()]);
     } 
-
+    function switchToCalendar() {
+      setShowCalendar(true);
+      setShowEvents(false);
+    }
+    function switchToEvents() {
+      setShowCalendar(false);
+      setShowEvents(true);
+    }
+    
+    function showEditModal(event:CalendarEvent|null)
+    {
+        setEditModal(!editModal);
+        setEvent(event);
+    }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.background}}>
+      
         {/*Date and Today Button*/}
         <View style={calendarTheme.header}>
             <View style = {calendarTheme.dateContainer}>
@@ -79,27 +120,65 @@ export default function CalendarPage() {
                 </TouchableOpacity>
             </View>
         </View>
+        
         {/*Calendar & Events Tabs */}
         <View style={calendarTheme.tabs}>
-            <Text style={calendarTheme.tabsText} onPress={() => setCurrentMode('week')}>Calendar</Text>
-            <Text style={calendarTheme.tabsText} onPress={() => setCurrentMode('schedule')}>Events</Text>
+            <Text style={calendarTheme.tabsText} onPress={() => switchToCalendar()}>Calendar</Text>
+            <Text style={calendarTheme.tabsText} onPress={() => switchToEvents()}>Events</Text>
         </View>
+        
         {/*Calendar*/}
-        <Calendar
-            events={events}
-            height={SCREEN_HEIGHT}
-            date = {currentDate}
-            eventCellStyle = {calendarTheme.eventStyle}
-            mode={currentMode}
-            onPressDateHeader={(date:Date) =>changeView(date, true)}
-            onSwipeEnd = {(date:Date) => changeView(date, false)}
-            theme = {theme.calendar}
-        />
-
-        {/*Create Event Modal*/}
-        <ModalForm title ="Create Event">
+        <View
+          style={{ flex: 1 }}
+          onLayout={calendarLayout}
+        >
+          {showCalendar && (<Calendar
+              events={events}
+              height={calendarHeight}
+              date = {currentDate}
+              eventCellStyle = {calendarTheme.eventStyle}
+              mode={currentMode}
+              onPressDateHeader={(date:Date) =>changeView(date, true)}
+              onSwipeEnd = {(date:Date) => changeView(date, false)}
+              theme = {theme.calendar}
+              onPressEvent={(event) => showEditModal( event as CalendarEvent)}
+          />)
+          }
+          {/* Event View - Approval vs Approved*/}
+          {/*
+            //TODO: Need to edit "Your Events" to check user against event owner
+            //TODO: Add in filters -- Filter events by creator 
+          */}
+          {showEvents && (
+            <ScrollView style={calendarTheme.indent}>
+              <ThemedText type='subtitle'>Needs Approval</ThemedText>
+              {
+                events.map((event) =>  {
+                  if(event.needsApproval.includes('me'))
+                  {
+                    return  <EventTile title={event.title} start={event.start} end={event.end} description ={event.description} needsApproval= {event.needsApproval} />;
+                  }
+                })
+              }
+              <ThemedText type='subtitle'>Your Events</ThemedText>
+              {
+                events.map((event) =>  {
+                  if(!event.needsApproval.includes('me'))
+                  {
+                    return  <EventTile title={event.title} start={event.start} end={event.end} description ={event.description} needsApproval= {event.needsApproval}/>;
+                  }
+                })
+              }
+            </ScrollView>
+          )}
           
-        </ModalForm>
+        </View>
+        {/* Create Event Modal */}
+        <ModalCalendarForm formTitle ="Create Event" edit={false} onClose={() => setEditModal(false)} />
+
+        {editModal && (
+          <ModalCalendarForm formTitle="Edit Event" edit={true} event={event} onClose={() => setEditModal(false)}/>
+          )}
     </SafeAreaView>
   )
 }
@@ -174,5 +253,26 @@ const calendarTheme = StyleSheet.create({
   },
   eventStyle: {
     backgroundColor: '#4DC591',
+    borderColor: '#12935b',
+    borderWidth: 1,
   },
+  dateTimePicker: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderWidth: 4,
+    borderColor: "#ba1a1a"
+  },
+  dateTime: {
+    height: 36,
+    backgroundColor: 'rgba(237, 237, 237, 1)',
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    justifyContent: 'center',
+    width: 100
+  },
+  indent: {
+    marginLeft: 10
+  }
 });
