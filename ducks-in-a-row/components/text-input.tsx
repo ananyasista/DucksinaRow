5da68276d0a134474207441776609ed80d975d6b
Sofia Lynch <113479969+SofiaBLynch@users.noreply.github.com>
2026-02-28 13:49:57 -0500

    Create Event form basic layout almost complete; Missing start/end time inputs

--- ducks-in-a-row/components/text-input.tsx
+++ ducks-in-a-row/components/text-input.tsx
@@ -1,102 +1,43 @@
-import { Calendar, Mode } from 'react-native-big-calendar'
-import { StyleSheet, Dimensions, TouchableOpacity, Modal} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react';
-// import { View } from 'react-native-reanimated/lib/typescript/Animated';
-import { View, Text } from 'react-native';
-import { Button, Header } from '@react-navigation/elements';
-import Octicons from "@expo/vector-icons/Octicons";
 import { PropsWithChildren } from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-type ModalProps = PropsWithChildren<{
-    title:string;
+type ThemedTextInputProps = PropsWithChildren<{
+    size?: 'small' | 'large';
+    multiline?: boolean,
+    placeholder?: string,
 }>;
-export default function ModalForm(props: ModalProps) {
-    const[addVisible, setAddVisible] = useState(false);
+
+export function ThemedTextInput({size='small', multiline = false, placeholder= ""}: ThemedTextInputProps) {
 
   return (
-    <SafeAreaView style={{ flex: 1}}>
-        {/*Floating Add Button*/}
-        <View style = {modalTheme.addButton}>
-            <TouchableOpacity onPress={() => setAddVisible(true)}>
-                <Octicons name='plus' size = {30} color='#fff'/> 
-            </TouchableOpacity>
-        </View>
-
-        <Modal 
-            animationType="slide"
-            visible={addVisible}
-            presentationStyle='formSheet'
-            allowSwipeDismissal = {true}
-            onRequestClose = {() => setAddVisible(false)} 
-        >
-            <View style={modalTheme.header}>
-                <TouchableOpacity style={modalTheme.cancelButton} onPress={() => setAddVisible(false)}>
-                    <Text style={modalTheme.cancelText}>Cancel</Text>
-                </TouchableOpacity>
-                <Text style={modalTheme.headerText}>{props.title}</Text>
-                <TouchableOpacity style={modalTheme.saveButton} onPress={() => setAddVisible(false)}>
-                    <Text style={modalTheme.saveText}>Save</Text>
-                </TouchableOpacity>
-            </View>
-            <View style= {{flex: 1, padding: 16}}>
-                {props.children}
-            </View>
-        </Modal>
-    </SafeAreaView>
-  )
+    <TextInput
+        style={[
+            styles.textInput,
+            size === 'small' ? styles.small : undefined,
+            size === 'large' ? styles.large : undefined
+        ]}
+        multiline = {multiline}
+        placeholder= {placeholder}
+        placeholderTextColor = 'rgba(171, 164, 164, 0.58)'
+    /> 
+  );
 }
 
-const modalTheme = StyleSheet.create({
-    header: {
-        justifyContent:"space-between",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        padding: 12
-    },
-    headerText: {
-        fontSize: 24,
-        fontWeight: 600
-    },
-    addButton: {
-        backgroundColor: '#4DC591',
-        width: 50,
-        height: 50,
-        borderRadius: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        bottom: 40,
-        right: 30,
-    },
-    cancelButton: {
-        backgroundColor: '#fff',
-        borderWidth: 2,
-        borderRadius: 10,
-        color: '#000',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 100,
-        height: 50
-    },
-    cancelText: {
-        color: '#000',
-        fontSize: 16,
-        fontWeight: 500
-    },
-    saveButton: {
-        backgroundColor: '#000',
-        borderRadius: 10,
-        color: '#fff',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 100,
-        height: 50
-    },
-    saveText: {
-        color: "#fff",
-        fontSize: 16,
-        fontWeight: 500
-    }
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    marginTop: 10, 
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: '#ABA4A461',
+    backgroundColor: '#F6F4F4C4',
+  },
+  small: {
+    height: 32,
+  },
+  large: {
+    height:102,
+  }
 });
