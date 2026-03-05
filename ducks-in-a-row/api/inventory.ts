e47b845b4aafe841535f31e245d360f67e36cd40
Leyna Huynh <leynarhuynh@gmail.com>
2026-03-04 21:07:37 -0500

    Adjust inventory token access

--- ducks-in-a-row/api/inventory.ts
+++ ducks-in-a-row/api/inventory.ts
@@ -4,7 +4,7 @@ export interface InventoryCard {
   id: string;
   name: string;
   restock_needed: boolean;
-  quantity : string;
+  quantity: string;
   last_purchased_by: {
     id: string;
     name: string;
@@ -18,51 +18,34 @@ export interface InventoryDetails extends InventoryCard {
   last_purchase_date: string | null;
 }
 
-export async function inventory(token: string) {
-  const res = await api.get("/api/inventory", {
-    headers: { Authorization: `Token ${token}` },
-  });
-  return res.data;
-}
-
-export const getInventory = async (token: string, filters?: {
+export const getInventory = async (filters?: {
   restock_needed?: boolean;
   purchased_by?: string;
   location?: string;
 }) => {
   const response = await api.get<InventoryCard[]>("/inventory/", {
-    headers: { Authorization: `Token ${token}` },
     params: filters,
   });
-
   return response.data;
 };
 
-export const getItemById = async (token: string, id: string) => {
-  const response = await api.get<InventoryDetails>(`/inventory/${id}/`, {
-    headers: { Authorization: `Token ${token}` },
-  });
+export const getItemById = async (id: string) => {
+  const response = await api.get<InventoryDetails>(`/inventory/${id}/`);
   return response.data;
 };
 
-export const createItem = async (token: string, data: any) => {
-  const response = await api.post("/inventory/", data, {
-    headers: { Authorization: `Token ${token}` },
-  });
+export const createItem = async (data: any) => {
+  const response = await api.post("/inventory/", data);
   return response.data;
 };
 
-export const updateChore = async (token: string, id: string, data: any) => {
-  const response = await api.patch(`/chores/${id}/`, data, {
-    headers: { Authorization: `Token ${token}` },
-  });
+export const updateItem = async (id: string, data: any) => {
+  const response = await api.patch(`/inventory/${id}/`, data);
   return response.data;
 };
 
-export const deleteItem = async (token: string, id: string) => {
-  await api.delete(`/inventory/${id}/`, {
-    headers: { Authorization: `Token ${token}` },
-  });
+export const deleteItem = async (id: string) => {
+  await api.delete(`/inventory/${id}/`);
 };
 
 export const getInventoryFilterOptions = async () => {
