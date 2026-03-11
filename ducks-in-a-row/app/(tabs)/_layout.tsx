b747061bffd68c4302980c3ba28f3d8c561fbfd2
Ananya Sista <sistaa@ufl.edu>
2026-03-11 15:01:31 -0400

    Finished chores page, filter modal, add/edit/view modal

--- ducks-in-a-row/app/(tabs)/_layout.tsx
+++ ducks-in-a-row/app/(tabs)/_layout.tsx
@@ -37,6 +37,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="calendar.circle.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="chores"
+        options={{
+          title: 'Chores',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.bullet.clipboard.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
